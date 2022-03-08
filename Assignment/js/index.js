030ab1d96a15c6060050bd9ddeefbb30d229e12e
czamora7 <czamora4@ewu.edu>
2022-03-08 14:46:14 -0800

    wrote joke retrieval and event listener

--- Assignment/js/index.js
+++ Assignment/js/index.js
@@ -5,12 +5,37 @@ axios({
 })
     .then(function (response) {
         console.log(response);
+        console.log(response.data.joke)
 
+        let jokeText = document.querySelector(".joke");
+        jokeText.innerText = response.data.joke;
     })
 
     .catch(function (error) {
-        console.log(error)
+        console.log(error);
 
-        let jokeText = document.
+        let jokeError = document.querySelector(".error");
+        jokeError.innerText = "Please try again in a few moments.";
     })
+
 //add an event listener (click) to tell another joke
+document.getElementsByClassName("jokecard_another").addEventListener("click", function () {
+    axios({
+        method: 'get',
+        url: 'https://v2.jokeapi.dev/joke/Programming'
+    })
+        .then(function (response) {
+            console.log(response);
+            console.log(response.data.joke)
+
+            let jokeText = document.querySelector(".joke");
+            jokeText.innerText = response.data.joke;
+        })
+
+        .catch(function (error) {
+            console.log(error);
+
+            let jokeError = document.querySelector(".error");
+            jokeError.innerText = "Please try again in a few moments.";
+        })
+});
